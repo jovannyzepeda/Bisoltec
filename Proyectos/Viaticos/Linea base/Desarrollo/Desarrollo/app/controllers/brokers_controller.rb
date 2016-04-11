be6cf62aff512581ec6f17f0f813224c7ec201c0
nachomartinez30 <ignacio3010@hotmail.com>
2016-04-11 11:18:44 -0500

    Linea Base Desarrollo

---
+++ Proyectos/Viaticos/Linea base/Desarrollo/Desarrollo/app/controllers/brokers_controller.rb
@@ -0,0 +1,68 @@
+class BrokersController < ApplicationController
+  before_action :set_broker, only: [:destroy]
+  before_action :auth
+  before_action :set_proyect
+
+  # GET /brokers.json
+
+
+
+  # GET /brokers/new
+  def new
+    @broker = Broker.new
+    @notuser = Array.new(User.joins(:brokers).proyecto(@proyect))
+    @user = Array.new(User.usuario_inactivo)
+   
+    @notuser.each do |s|
+      @user.delete_if {|x| x.id == s.id }
+    end
+  end
+
+  # GET /brokers/1/edit
+
+
+  # POST /brokers
+  # POST /brokers.json
+  def create
+    @broker = Broker.new(broker_params)
+    @broker.proyect = @proyect
+    respond_to do |format|
+      if @broker.save
+        format.html { redirect_to [@zone,@proyect], notice: 'Broker agregado al proyecto.' }
+        format.json { render :show, status: :created, location: @broker }
+      else
+        format.html { render :new }
+        format.json { render json: @broker.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /brokers/1
+  # PATCH/PUT /brokers/1.json
+
+
+  # DELETE /brokers/1
+  # DELETE /brokers/1.json
+  def destroy
+    @broker.destroy
+    respond_to do |format|
+      format.html { redirect_to [@zone,@proyect], notice: 'Broker eliminado del proyecto.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_broker
+      @broker = Broker.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def broker_params
+      params.require(:broker).permit(:user_id, :proyect_id)
+    end
+    def set_proyect
+      @proyect = Proyect.find(params[:proyect_id])
+      @zone = Zone.find(params[:zone_id])
+    end
+end
---
+++ Proyectos/Viaticos/Linea base/Desarrollo/Desarrollo/app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+	before_action :auth
+
+  def index
+
+  end 
+end
